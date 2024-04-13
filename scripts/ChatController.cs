ff8f7acb951049e69f7e2714250a33f53df6df5c
Florian Dueller <fpw.dueller@gmail.com>
2024-04-13 21:47:03 +0200

    Fix for chat input

--- scripts/ChatController.cs
+++ scripts/ChatController.cs
@@ -16,7 +16,20 @@ public partial class ChatController : Control
         _chatButton = GetNode<Button>("%ChatButton");
         _chat = GetNode<RichTextLabel>("%Chat");
         _chatInput = GetNode<LineEdit>("%ChatInput");
-        _chatButton.Pressed += () => { AddMessage($"[color=#a530F0]Satan[/color]: {_chatInput.Text}\n"); GameController.TriggerResponseFlood(); };
+        _chatInput.TextSubmitted += OnChatEditOnTextSubmitted;
+        _chatButton.Pressed += OnChatButtonOnPressed;
+    }
+
+    private void OnChatEditOnTextSubmitted(string newText)
+    {
+            OnChatButtonOnPressed();
+    }
+    
+    private void OnChatButtonOnPressed()
+    {
+        AddMessage($"[color=#a530F0]Satan[/color]: {_chatInput.Text}\n");
+        _chatInput.Text = "";
+        GameController.TriggerResponseFlood();
     }
 
     public override void _Process(double delta)
